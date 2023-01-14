0d357fef17edeb8cce05037925866ad96967cdf0
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-01-14 18:54:46 +0900

    feat: FCM 기능 추가

--- src/scripts/realtime.py
+++ src/scripts/realtime.py
@@ -1,9 +1,14 @@
+import os
+
 from aiohttp import ClientTimeout, ClientSession
+from pyfcm import FCMNotification
 from sqlalchemy import delete, insert
 from sqlalchemy.orm import Session
 
 from models import ReadingRoom
 
+firebase_key = os.getenv("FIREBASE_TOKEN")
+push_service = FCMNotification(api_key=firebase_key)
 
 async def get_realtime_data(db_session: Session) -> None:
     url = "https://lib.hanyang.ac.kr/smufu-api/pc/0/rooms-status"
@@ -14,6 +19,14 @@ async def get_realtime_data(db_session: Session) -> None:
             response_json = await response.json()
             room_list = response_json["data"]["list"]
             for room in room_list:
+                if room["available"] > 0:
+                    data = {
+                        "body": f"{room['name']}에 좌석이 {room['available']}개 남았습니다.",
+                        "title": "열람실 좌석 발견!",
+                    }
+                    push_service.notify_topic_subscribers(
+                        topic_name=f"reading_room_{room['id']}", data_message=data
+                    )
                 room_items.append(dict(
                     campus_id=room["branchGroup"]["id"],
                     room_id=room["id"],
