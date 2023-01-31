8cad9ad4c910a24733a973cf8e9031a4b0d9cc8f
Jeongin Lee <jil8885@hanyang.ac.kr>
2023-01-31 16:51:54 +0900

    feat: 마스터 노드를 활용하기 위한 코드 구현

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import asyncio
 
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import sessionmaker
 
 from scripts.realtime import get_realtime_data
-from utils.database import get_db_engine
+from utils.database import get_db_engine, get_master_db_engine
 
 
 async def main():
@@ -12,6 +13,16 @@ async def main():
     session = session_constructor()
     if session is None:
         raise RuntimeError("Failed to get db session")
+    try:
+        await execute_script(session)
+    except OperationalError:
+        connection = get_master_db_engine()
+        session_constructor = sessionmaker(bind=connection)
+        session = session_constructor()
+        await execute_script(session)
+
+
+async def execute_script(session):
     job_list = [get_realtime_data(session)]
     await asyncio.gather(*job_list)
     session.close()
